f99d9c10d06dcf3bde1d6fd6835631d92ba67d59
surbhi <surbhi.midha01@gmail.com>
2020-04-30 15:13:51 +1000

    Added Layout, hoc, made folder str

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import Layout from './components/Layout/Layout'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        hello
+      <div>
+          <Layout>
+            <p>Test children</p>
+          </Layout>
       </div>
     );
   }
