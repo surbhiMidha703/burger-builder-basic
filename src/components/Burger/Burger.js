d6fe84cf3ff68163ca8d3aaa05fae92dac6a9e73
surbhi <surbhi.midha01@gmail.com>
2020-04-30 18:42:11 +1000

    reduce the array of BurgerIngredients to a single array using reduce js function. Added msg if there no ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,22 @@ import React from 'react'
 import styles from './Burger.css'
 
 const Burger = props => { 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map((igKey) =>{
                 return [...Array(props.ingredients[igKey])].map((_, i) => {
                     return <BurgerIngredient type={igKey} key={igKey+i}/>
                 })
             })
-        
-  
+        .reduce((arr, ele) => {
+            console.log("ele=>  ",ele)
+            return arr.concat(ele)
+        }, [])
+
+       if(transformedIngredients.length === 0){
+            transformedIngredients = <p> Please start adding the ingredients</p>
+       }
+
+       console.log('transformedIngredients:  ',transformedIngredients)
         return <div className={styles.Burger}>
                 <BurgerIngredient type='bread-top' />
                   {transformedIngredients}
