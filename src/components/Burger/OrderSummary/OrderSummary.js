ea47f8cbf0a81badd4206c73e70a93a1c8714ce1
surbhi <surbhi.midha01@gmail.com>
2020-05-16 20:30:48 +1000

    Added cancel/continue button handler and css. Display total price in summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Aux from '../../../hoc/Aux'
+import Button from '../../UI/Button/Button'
 
 const OrderSummary = props => {
     const ingredientSummary =  Object.keys(props.ingredients)
@@ -17,9 +18,10 @@ const OrderSummary = props => {
         <ul>
             {ingredientSummary}
         </ul>
+        <p><strong> Total Price: </strong>{props.price.toFixed(2)}</p>
         <p>Continue to Checkout</p>
-        <button>CANCEL</button>
-        <button>CONTINUE</button>
+        <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
     </Aux>
     )
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,6 +85,10 @@ class BurgerBuilder extends Component{
         this.setState({orderNowClicked: false})
     }
     
+    purchaseContinueHandler = () => {
+        alert('Please continue!')
+    }
+
     render(){
         const disabledInfo = {
             ...this.state.ingredients
@@ -96,7 +100,12 @@ class BurgerBuilder extends Component{
         return(
             <Aux>
                 <Modal show={this.state.orderNowClicked} modalClosed={this.purchaseCancelled}>
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                    <OrderSummary
+                        ingredients={this.state.ingredients}
+                        purchaseCancelled={this.purchaseCancelled}
+                        purchaseContinued={this.purchaseContinueHandler}
+                        price={this.state.totalPrice}
+                    />
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
