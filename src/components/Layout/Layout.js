c79e6d9ac614dbbbbf7c5d8ac64ce4dd40dc45e5
surbhi <surbhi.midha01@gmail.com>
2020-05-17 20:07:40 +1000

    Created side drawer to hold navigation items for mobilr

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Aux from '../../hoc/Aux'
 import styles from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = props => (
     <Aux>
         <Toolbar />
+        <SideDrawer/>
         <main className={styles.Content} >
              {props.children}
         </main>
