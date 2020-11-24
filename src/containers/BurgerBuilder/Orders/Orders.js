05696f027c263410f0ffc970e675f256caccfbdf
surbhi <surbhi.midha01@gmail.com>
2020-11-24 19:56:32 +1100

    Fetched orders from firebase using axios. Converted key:value pairs gotton from axios to array of orders objects with id in it

--- src/containers/BurgerBuilder/Orders/Orders.js
+++ src/containers/BurgerBuilder/Orders/Orders.js
@@ -1,9 +1,39 @@
+import axios from '../../../axios-orders'
 import React, {Component} from 'react'
 import Order from '../../../components/Order/CheckoutSummary/Order'
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
 
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true,
+    }
+    componentDidMount() {
+        console.log('orders mount')
+        axios.get('/orders.json')
+            .then(res => {
+                console.log('res.data=>  ',res.data)
+                const fetchedOrders = []
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    })
+                }
+
+                console.log('fetchedOrdrs=> ',fetchedOrders)
+                this.setState({orders: fetchedOrders})
+                this.setState({loading: false})
+            })
+            .catch(err => {
+                console.log('err=> ',err)
+                this.setState({loading: false})
+            })
+    }
+
     render() {
+        console.log('orders rendered')
         return(
             <div>
                 <Order/>
@@ -12,4 +42,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders
+export default withErrorHandler(Orders, axios)
