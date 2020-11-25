223b3d4e4c1274ff2d6a044873a0ee71ffbf5f2d
surbhi <surbhi.midha01@gmail.com>
2020-11-25 20:14:23 +1100

    Extracted form data in an array, loop through array and pass the data to input elements to render dynamically

--- src/containers/BurgerBuilder/Checkout/ContactData/ContactData.js
+++ src/containers/BurgerBuilder/Checkout/ContactData/ContactData.js
@@ -83,12 +83,24 @@ class ContactData extends Component {
     }
 
     render(){
-        let form = (<form>
-            <Input inputtype='input' type='text' name='name' placeholder='Your name'/>
 
-            <Input elementType='' elementConfig='' value=''/>
-            <Input inputtype='input' type='text' name='street' placeholder='Street'/>
-            <Input inputtype='input' type='text' name='postal' placeholder='Postal address'/>
+        const formElementsArray = []
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key],
+            })
+        }
+
+        let form = (<form>
+            {formElementsArray.map(formElement => (
+                <Input
+                    key={formElement.id}
+                    elementType={formElement.config.elementType}
+                    elementConfig={formElement.config.elementConfig}
+                    value={formElement.config.value}
+                    />
+            ))}
             <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
         </form>)
 
